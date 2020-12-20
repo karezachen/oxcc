9a577d440aefc3706b9c7995fc04f57d0e4f892e
陈 亿强 <kareza@qq.com>
2020-12-21 02:13:53 +0800

    添加测试输出的代码

--- reply.py
+++ reply.py
@@ -61,9 +61,13 @@ class PunchTheClock(Msg):
         self.__dict['CreateTime'] = int(time.time())
         postUrl = ("http://wx.kareza.cn:8080/lifeplus/histron/punchTheClock?job_number=%s" % content)
         urlResp = urllib.request.urlopen(postUrl)
+        print('urlResp: ' + urlResp)
         urlResp = urlResp.decode()
+        print('urlResp: ' + urlResp)
         urlResp = json.loads(urlResp)
+        print('urlResp: ' + urlResp)
         data = urlResp['data']
+        print('data: ' + data)
         self.__dict['Content'] = data
 
     def send(self):
