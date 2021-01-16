83c9baf809832df55a19ce7a200bef454a69d0e4
Kareza <kareza@qq.com>
2021-01-16 21:20:59 +0800

    为主函数添加注释

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
+# author: kareza
+# email: kareza@qq.com
+# description: 主函数
 
 import web
-# from handle import Handle
-from authentication import Handle
+from handle import Handle
+# from authentication import Handle
 
 urls = (
     '/wechat', 'Handle',
