daaae91593180a84382c4aa982fca1f6ddaa05a1
Kareza <kareza@qq.com>
2021-01-17 20:07:31 +0800

    修改服务启动主函数路径

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,5 +4,5 @@
 # description: 后台启动服务，并保存进程pid
 
 binpath=$(dirname $0)
-nohup python3 -u $binpath/../lib/main.py 8080 > $binpath/../logs/server.log 2>&1 &
+nohup python3 -u $binpath/../code/main.py 8080 > $binpath/../logs/server.log 2>&1 &
 ps aux | grep '[0-9] python3 .*main.py 8080' | awk '{print $2}' > $binpath/../logs/pid
